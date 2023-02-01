e7ce6a83181f5362c318729e178b81050e84a0de
dutenrapha <dutenrapha@hotmail.com>
2023-02-01 17:55:46 -0300

    lista 2 ex07 parse complete bug on logic

--- lista2/ex07/periodic_table.py
+++ lista2/ex07/periodic_table.py
@@ -10,12 +10,13 @@ def doctype():
         <title>Document</title>
     </head>
     <body>
-        {{placeholder}}
+        placeholder
     </body>
     </html>'''
 
 def insert_placeholder(base_html, placeholder, new_html):
-    return base_html.replace(placeholder,new_html)
+    resp = base_html.replace(placeholder,new_html)
+    return resp
 
 def html_box_empty():
     return '''<td style="border: 1px solid black; padding:10px">
@@ -25,30 +26,44 @@ def html_box_empty():
 def html_box_full(name, number, small, molar, electron):
     return f"<td style=\"border: 1px solid black; padding:10px\"><h4>{name}</h4><ul><li>No {number}</li><li>{small}</li><li>{molar}</li><li>{electron}</li><ul></td>"
 
-def html_table(rows, columns, empty_list=[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17],[2,3,4,5,6,7,8,9,10,11],[2,3,4,5,6,7,8,9,10,11],[],[],[],[]]):
-    base = '''<table> placeholder </table>'''
+def html_table(rows, columns):
+    base = '''<table>
+            placeholder
+        </table>'''
     r = ""
     for row in range(rows):
-        r = r + f"<tr> row_{row} </tr>"
+        r = r  + f"<tr> row_{row} </tr>"
     base = insert_placeholder(base, "placeholder", r)
-
     for row in range(rows):
         c = ""
         for column in range(columns):
-            if column in empty_list[row]:
-                c = c + " box_empty"
-            else:
-                c = c + f" box_{row}_{column}"
+                c = c + f"row_{row}_{column} "
         base = insert_placeholder(base, f"row_{row}", c)
     return(base)
 
+def extract(str, tpy="key"):
+    if tpy == "key":
+        return(str.split("=")[0].strip())
+    elif tpy == "position":
+        return(int(str.split(":")[1].strip()))
+    elif tpy == "value":
+        return(str.split(":")[1])
+    else:
+        return(None)
+
 def read_txt():
+    d = {}
     with open('periodic_table.txt') as f:
         lines = f.readlines()
         for line in lines:
             l = line.strip()
             l = l.split(",")
-            print(l)
+            d[extract(l[0])] = {"position": extract(l[0], "position"),
+                                "number": extract(l[1], "value"),
+                                "small": extract(l[2], "value"),
+                                "molar": extract(l[3], "value"),
+                                "electron": extract(l[4], "value")}
+    return(d)
 
 def generate_html(html):
     file_html = open("periodic_table.html", "w")
@@ -56,13 +71,18 @@ def generate_html(html):
     file_html.close()
 
 if __name__ == '__main__':
-    # html = html_table(7,18)
-    # html = insert_placeholder(html, "box_empty", html_box_empty())
-    # for row in range(7):
-    #     for column in range(18):
-    #         html = insert_placeholder(html, f"box_{row}_{column}", html_box_full())
-    read_txt()
-    # html_t = insert_placeholder(html_table(3,10), "{{box}}", html_box_full())
-    # html_t = insert_placeholder(html_t, "{{box_empty}}", html_box_empty())
-    # html = insert_placeholder(doctype(), "{{placeholder}}", html_t)
-    # generate_html(html)
+    d = read_txt()
+    row = 0
+    html = html_table(7,18)
+    for k, v in d.items():
+        print(f"row_{row}_{v['position']}")
+        html_box = html_box_full(name=k, number=v['number'], small=v['small'], molar=v['molar'], electron=v['electron'])
+        temp = insert_placeholder(html, f"row_{row}_{v['position']}", html_box)
+        html = temp
+
+        if v['position'] == 17:
+            row += 1
+        
+    
+    html = insert_placeholder(doctype(), "placeholder", html)
+    generate_html(html)
